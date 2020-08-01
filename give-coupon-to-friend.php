f61d83adc5deb74e7fe5d118ef52e0025aaeb87c
Xavier López <lopezx@gmail.com>
2020-08-01 14:04:46 +0200

    commit to wordpress.org

--- give-coupon-to-friend.php
+++ give-coupon-to-friend.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name: Give Coupon to Friend
- * Plugin URI: https://github.com/xavierlopez/give-coupon-to-friend
- * Description: Automatically generate a coupon to give to a friend when order is completed.
- * Version: 1.0.0
- * Author: Xavier Lopez
- * Author URI: https://xavierlopez.dev
- * Text domain: give-coupon-to-friend
- * WC requires at least: 3.0.0
- * WC tested up to: 4.3
- */
+* Plugin Name: Give Coupon to Friend
+* Plugin URI: https://github.com/xavierlopez/give-coupon-to-friend
+* Description: Automatically generate a coupon to give to a friend when order is completed.
+* Version: 1.0.0
+* Author: Xavier Lopez
+* Author URI: https://xavierlopez.dev
+* Text domain: give-coupon-to-friend
+* WC requires at least: 3.0.0
+* WC tested up to: 4.3.0
+*/
 
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -31,80 +31,100 @@ if ( !(in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins',
 
 
 /*
-  * Load translation files
-  */
-  add_action('plugins_loaded', 'gctf_load_translations');
-  function gctf_load_translations() {
-	  load_plugin_textdomain('give-coupon-to-friend', false, 'give-coupon-to-friend/languages');
-  }
+* Load translation files
+*/
+add_action('plugins_loaded', 'gctf_load_translations');
+function gctf_load_translations() {
+	load_plugin_textdomain('give-coupon-to-friend', false, 'give-coupon-to-friend/languages');
+}
   
-
 
 /*
 * Register submenu admin item under WooCommerce
 */
 add_action('admin_menu', 'gctf_register_plugin_settings',99);
 function gctf_register_plugin_settings() {
-    add_submenu_page( 'woocommerce', 'Give Coupon to Friend', 'Give Coupon to Friend', 'manage_options', 'give-coupon-to-friend', 'gctf_submenu_page_callback' ); 
-    
+	if (current_user_can( 'edit_posts' )) {
+		add_submenu_page( 'woocommerce', 'Give Coupon to Friend', 'Give Coupon to Friend', 'manage_options', 'give-coupon-to-friend', 'gctf_submenu_page_settings' );  
+	}
 }
 
 
 /*
 * Settings page callback function
 */
-function gctf_submenu_page_callback() {
-
-    if (array_key_exists('give-coupon-to-friend',$_POST)) {
-        update_option('gctf_text_coupon', $_POST['text_coupon']);
-    ?>
-       <div id ="settings-error-settings_updated" class="updated settings-error note is-dismissable">
-           <strong><?php _e('Settings have been saved.','give-coupon-to-friend');?></strong>
-    </div> 
-    <?php 
-    } 
-    
-    $text_coupon = get_option('gctf_text_coupon','Give this coupon to a friend:');
-    ?>
-
-    <div class="wrap">
-    <h1>Give Coupon to Friend Plugin Settings</h1>
-    <p> <?php _e('The coupon amount will be 100% of the order total.','give-coupon-to-friend'); ?>
-    <form method="post" action="">
-        <label for="text_coupon">
-        <h3><?php _e('Text to show in Thank You Page and Customer Email','give-coupon-to-friend')?>:</h3>
-        <textarea name="text_coupon" class="large-text"> <?php echo $text_coupon ?></textarea>    
-        <input type="submit" name="give-coupon-to-friend" class="button button-primary">
-    </form>
-    </div>
-<?php 
+function gctf_submenu_page_settings() {
+		if (array_key_exists('give-coupon-to-friend',$_POST)) {	
+			if (! isset( $_POST['gctf-nonce'] ) || ! wp_verify_nonce( $_POST['gctf-nonce'], 'give-coupon-to-friend' ) ) {
+			   print 'Sorry, your nonce did not verify.'; exit;
+			} else {
+				if ( !empty($_POST['gctf_text_coupon']) ) { 
+					$text_to_display = sanitize_textarea_field($_POST['gctf_text_coupon']);
+					if (current_user_can( 'edit_posts' )) {
+						update_option('gctf_text_coupon', $text_to_display);
+					}
+				?>
+				<div id ="settings-error-settings_updated" class="updated settings-error note is-dismissable">
+					<strong><?php esc_html_e('Settings have been saved.','give-coupon-to-friend');?></strong>
+				</div> 
+				<?php
+				} 
+			}
+		} 
+		
+		$text_coupon = get_option('gctf_text_coupon','Give this coupon to a friend:');
+		?>
+		<div class="wrap">
+		<h1>Give Coupon to Friend Plugin Settings</h1>
+		<p> <?php esc_html_e('The coupon amount will be 100% of the order total.','give-coupon-to-friend'); ?>
+		<form method="post" action="">
+			<label for="text_coupon">
+			<h3><?php esc_html_e('Text to show in Thank You Page and Customer Email','give-coupon-to-friend')?>:</h3>
+			<textarea name="gctf_text_coupon" class="large-text"> <?php echo esc_textarea( $text_coupon); ?></textarea>    
+			<input type="submit" name="give-coupon-to-friend" class="button button-primary">
+			<?php wp_nonce_field( 'give-coupon-to-friend', 'gctf-nonce' ); ?>
+		</form>
+		</div>
+	<?php 
 } 
-
 
 
 /**
 * Generate coupon on order complete
 */
-add_action( 'woocommerce_checkout_update_order_meta', 'gctf_generate_coupon', 15, 1 );
-function gctf_generate_coupon( $order_id ) { 
+add_action( 'woocommerce_checkout_update_order_meta', 'gctf_generate_coupon_for_friend', 15, 1 );
+function gctf_generate_coupon_for_friend( $order_id ) { 
 
-	$order = wc_get_order( $order_id );
-	$amount = $order->get_total(); // Amount
-	
+	if (isset($order_id)) {
+		$order = wc_get_order( $order_id );
+		$amount = $order->get_total(); // Amount
+		$coupon_code = $order_id * rand(100, 999); // Random Code
+		$customer_email =  $order->get_billing_email(); // Customer billing email
+	}
+
 	//if order total is less than 1, return
 	if ( $amount < 1 ) {
         return;
     }
 
-	$customer_email =  $order->get_billing_email(); // Customer billing email
-
-	$coupon_code = $order_id * rand(100, 999); // Random Code
+	if (is_user_logged_in()) {
+		$current_user = wp_get_current_user();
+		$user_email = $current_user->user_email;
+ 	} else {
+		 $user_email = false;
+	}
 
 	// Get a new instance of the WC_Coupon object
 	$coupon = new WC_Coupon();
 
 	// Set the necessary coupon data
-	$coupon->set_code( $coupon_code );
+	
+	if (isset($coupon_code)) {
+		$coupon->set_code( $coupon_code );
+	}
+	if (isset($order_id)) {
+		$coupon->set_description(__( 'Automatically generated coupon with order number ', 'give-coupon-to-friend' ).$order_id);
+	}
 	$coupon->set_discount_type( 'fixed_cart' );
 	$coupon->set_amount( 1 );
 	$coupon->set_individual_use( true );
@@ -112,28 +132,32 @@ function gctf_generate_coupon( $order_id ) {
 	$coupon->set_usage_limit_per_user( 1 );
 	$coupon->set_limit_usage_to_x_items( 1 );
 	$coupon->set_date_expires( date( "Y-m-d H:i:s", strtotime('2021-07-31') ) );
-	$coupon->set_description(__( 'Automatically generated coupon with order number ', 'give-coupon-to-friend' ).$order_id);
-
+	
 	// Save the data
 	$post_id = $coupon->save();
 
-	// Add meta to coupon
-	update_post_meta( $post_id, 'coupon_amount', $amount );
-	update_post_meta( $post_id, 'email_creador', $customer_email );
-	// Add meta to order
-	update_post_meta( $order_id, 'cupon_generado', $coupon->get_code() );
-
+	if ($post_id) {
+		// Add meta to coupon
+		update_post_meta( $post_id, 'coupon_amount', $amount );
+		update_post_meta( $post_id, 'billing_email_creator', $customer_email );
+		update_post_meta( $post_id, 'user_email_creator', $user_email );
+	}
+	if ($order_id) {
+		// Add meta to order
+		update_post_meta( $order_id, 'generated_coupon', $coupon->get_code() );
+	}
 }; 
 
 
 /*
 * Show coupon on thankyou page.
 */
-add_action('woocommerce_thankyou', 'gctf_show_coupon',30,1);
-function gctf_show_coupon($order_id) {
-    if ( get_post_meta($order_id,'cupon_generado') ) {
-        echo "<div id='coupon_thankyou' style='border:2px solid;padding:4em'><h3 id='cupon_amigo'>".get_option('gctf_text_coupon','Give this coupon to a friend:'."<h3>");
-        echo "<h1 class='coupon_show'>".get_post_meta($order_id,'cupon_generado',true)."</h1></div>";
+add_action('woocommerce_thankyou', 'gctf_show_coupon_at_thankyou_page',30,1);
+function gctf_show_coupon_at_thankyou_page($order_id) {
+    if ( get_post_meta($order_id,'generated_coupon') ) {	
+		echo "<div id='coupon_thankyou'>";
+        echo "<h3 id='cupon_amigo'>".esc_html(get_option('gctf_text_coupon','Give this coupon to a friend:'))."</h3>";
+        echo "<h2 class='coupon_show'>".esc_html(get_post_meta($order_id,'generated_coupon',true))."</h2></div>";
     }
 };
 
@@ -142,13 +166,13 @@ function gctf_show_coupon($order_id) {
 /*
 * Show coupon on New Order email.
 */
-add_action( 'woocommerce_email_after_order_table', 'gctf_add_content_specific_email', 20, 4 );
-function gctf_add_content_specific_email( $order, $sent_to_admin, $plain_text, $email ) {
+add_action( 'woocommerce_email_after_order_table', 'gctf_show_coupon_at_email', 20, 4 );
+function gctf_show_coupon_at_email( $order, $sent_to_admin, $plain_text, $email ) {
     // if it is new order email AND there is a coupon generated on that order
-    if ( $email->id == 'customer_completed_order' &&  get_post_meta($order->get_id(),'cupon_generado') )  {
-        echo "<div style ='border:1px solid;margin-bottom:2em;padding:2em'><h2 id='cupon_email'>".get_option('gctf_text_coupon','Give this coupon to a friend:')."</h2>";
-	    echo "<h1 class='coupon_show'>".get_post_meta($order->get_id(),'cupon_generado',true)."</h1></div>";  
-    }
+    if ( $email->id == 'customer_completed_order' &&  get_post_meta($order->get_id(),'generated_coupon') )  {
+		echo "<p>".esc_html(get_option('gctf_text_coupon','Give this coupon to a friend:'))." <strong>".esc_html(get_post_meta($order->get_id(),'generated_coupon',true))."</strong></p>";  
+
+	}
 }
 
 
@@ -164,26 +188,34 @@ function gctf_validate_checkout_coupon($posted) {
 		return;
 	}
 
+	if (is_user_logged_in()) {
+		$current_user = wp_get_current_user();
+		$user_email = $current_user->user_email;
+ 	} else {
+		$user_email = false;
+	}
+
 	// instance of current cart
 	$carrito = WC()->cart;
 	
-	foreach ( $carrito->get_applied_coupons() as $coupon_code ) {
-		
-		// Retrieving the coupon ID
+	foreach ( $carrito->get_applied_coupons() as $coupon_code ) {	
 		$coupon_post_obj = get_page_by_title($coupon_code, OBJECT, 'shop_coupon');
 		$coupon_id       = $coupon_post_obj->ID;
 		
-		// Get an instance of WC_Coupon object in an array(necessary to use WC_Coupon methods)
-		$coupon = new WC_Coupon($coupon_id);
-		
-		// coupon creator
-		$creator = get_post_meta($coupon_id, 'email_creador', true);
+		if ($coupon_id) {
+			// Get an instance of WC_Coupon object in an array(necessary to use WC_Coupon methods)
+			$coupon = new WC_Coupon($coupon_id);
+			
+			// coupon creator
+			$billing_creator = get_post_meta($coupon_id, 'billing_email_creator', true);
+			$user_creator = get_post_meta($coupon_id, 'user_email_creator', true);
 
-		//  If the creator is using the coupon, remove coupon (This coupon is a gift for a friend, not for myself!)
-		if ($creator == $billing_email) {
-			$coupon->add_coupon_message( WC_Coupon::E_WC_COUPON_INVALID_REMOVED );
-			$carrito->remove_coupon($coupon_code);
-			$carrito->calculate_totals();
+			//  If the creator is using the coupon, remove coupon (This coupon is a gift for a friend, not for myself!)
+			if ( ($billing_creator == $billing_email) || ($billing_creator == $user_email) || ($user_creator == $billing_email) || ($user_creator == $user_email)) {
+				$coupon->add_coupon_message( WC_Coupon::E_WC_COUPON_INVALID_REMOVED );
+				$carrito->remove_coupon($coupon_code);
+				$carrito->calculate_totals();
+			}
 		}
 	}
 }
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Give Coupon to Friend ===
+Contributors: xavierdeveloper
+Tags: coupon, woocommerce, giveaway, give, regalar, present, wordofmouth
+Donate link: https://paypal.me/xavierlopezdev
+Requires at least: 4.0
+Tested up to: 5.4.2
+WC tested up to: 4.3.0
+Requires PHP: 5.6
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically generates a coupon to give to a friend when order is completed in WooCommerce.
+
+== Description ==
+
+Automatically generates a coupon to give to a friend when order is completed in WooCommerce (sort of a digital word-of-mouth).
+The coupon cannot be used by the person who ordered so it must be given to a friend.
+The value of the coupon is the same as the order that generated it. 
+I'm open to add customizations if you ask. Please email me at: xavierlopezdev@gmail.com
+
+Requirements:
+WooCommerce plugin must be active.
+
+== Installation ==
+1. Upload the entire 'give-coupon-to-friend' folder to the `/wp-content/plugins/` directory.
+1. Make sure 'WooCommerce' plugin is active before activating this plugin.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+
+== Frequently Asked Questions ==
+ 
+= Is this plugin free? =
+ 
+Yes, this plugin is totally free and my plan as the author is to implement more functionalities in the new future. 
+Please, any suggestions are welcome, just email me with your suggestions at: xavierlopezdev@gmail.com
+ 
+= Once the coupon is generated, can I limit its use? =
+ 
+Sure. Once the coupon is automatically generated after an order is completed, it can be fully customised as a normal coupon.
+
+= Where can I access the plugin settings? =
+ 
+You can access plugin settings under WooCommerce -> Give Coupon to Friend.
+
+
+== Screenshots ==
+
+1. Coupon shows in Thank You page.
+2. Coupon show in customer email.
+3. Plugin Settings.
+
+
+== Changelog ==
+
+1.0.0
+*First version of plugin.
